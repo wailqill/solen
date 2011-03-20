6c60b065f73b098729c1fca88defaefc0ea39d8a
Glenn Nilsson <glenn.nilsson@gmail.com>
2011-03-20 19:39:58 +0100

    Now shows current date on front page.

---
+++ features/front_page.feature
@@ -0,0 +1,13 @@
+Feature: Front page
+  In order to easy see the current status
+  As a user
+  I want to see the status of the current day
+  
+  @wip
+  Scenario: Todays date
+    Given I am on the front page
+    When I look at the page
+    Then I should see the current date
+  
+  
+  
---
+++ features/step_definitions/solen_steps.rb
@@ -0,0 +1,12 @@
+When /^I look at the page$/ do
+  # Do nothing. Empty placeholder
+end
+
+Then /^I should see the current date$/ do
+  date = I18n.l(Date.today, :format => :long)
+  if page.respond_to? :should
+    page.should have_xpath('//*', :text => date)
+  else
+    assert page.has_xpath?('//*', :text => date)
+  end
+end
